8487213431b0863758ed2649c6912fc4beb835fa
Logan Dequier <logann.dequier@gmail.com>
2025-01-15 15:57:24 +0100

    debut ajout affichage ressource

--- Modules/Mod_SAEProf/Modele_sae.php
+++ Modules/Mod_SAEProf/Modele_sae.php
@@ -2,7 +2,7 @@
 require_once 'connexion.php';
 
 class Modele_sae extends Connexion{
-    public function updateDescription($id_projet, $newDescription) {
+    public function majDescription($id_projet, $newDescription) {
         if ($id_projet > 0 && !empty($newDescription)) {
             try {
 
@@ -16,11 +16,39 @@ class Modele_sae extends Connexion{
         }
     }
 
+    public function ajouterRessource($id_projet, $titre, $description, $url) {
+        if ($id_projet > 0 && !empty($titre)) {
+            try {
+                $stmt = self::getBdd()->prepare("
+                    INSERT INTO ressources (projet_id, titre, description, url)
+                    VALUES (:projet_id, :titre, :description, :url)
+                ");
+                $stmt->bindParam(':projet_id', $id_projet, PDO::PARAM_INT);
+                $stmt->bindParam(':titre', $titre, PDO::PARAM_STR);
+                $stmt->bindParam(':description', $description, PDO::PARAM_STR);
+                $stmt->bindParam(':url', $url, PDO::PARAM_STR);
+                $stmt->execute();
+            } catch (Exception $e) {
+                die('Erreur : ' . $e->getMessage());
+            }
+        }
+    }
+    
+
     public function getProjet($id_Projet){
         $pdo_req = self::getBdd()->prepare("SELECT * FROM projets WHERE id = :id");
 		$pdo_req->bindParam(":id", $id_Projet, PDO::PARAM_INT);
 		$pdo_req->execute();
 		return $pdo_req->fetch(PDO::FETCH_ASSOC);
+    
+    }
+
+    public function getRessources($id_Projet){
+        $pdo_req = self::getBdd()->prepare("SELECT * FROM ressources WHERE projet_id = :projet_id");
+		$pdo_req->bindParam(":projet_id", $id_Projet);
+		$pdo_req->execute();
+		return $pdo_req->fetchAll();
+
     
     }
 }
